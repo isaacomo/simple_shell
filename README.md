175415dc6b19ffda756b01e4ca059792bb4c84ee
omorowange isaac <aas.isaac26@gmail.com>
2022-11-12 10:54:43 -0800

    0x16. C - Simple Shell

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a README.md Repository for 0x16. C - Simple Shell tasks.
---
+++ shell.c
@@ -0,0 +1,17 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <limits.h>
+#include <string.h>
+
+extern char **environ;
+
+char *find_command(char *command);
+#endif
